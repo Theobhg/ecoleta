d1bf6f91e4ce9c537cdd62967042483c025c20b2
Theobhg <assis.theo@gmail.com>
2020-06-01 13:37:58 -0300

    add json array example

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
-app.get('/users', () => {
+app.get('/users', (req, res) => {
   console.log('listagem de usuarios');
+
+  res.json([
+    'Diego',
+    'Cleiton',
+    'Robson'
+  ]);
 });
 
 app.listen(3333);
