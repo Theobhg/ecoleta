93e777cad638316f80620704984329223399c2e6
Theobhg <assis.theo@gmail.com>
2020-06-02 20:59:31 -0300

    pointscontrollers.index

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -1,7 +1,26 @@
-import { Request, Response, response } from 'express';
+import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query;
+
+    const parsedItems = String(items)
+    .split(',')
+    .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return res.json(points);
+  }
+
+
   async show(req: Request, res: Response) {
     const { id } = req.params;
 
@@ -11,9 +30,13 @@ class PointsController {
       return res.status(400).json({ message: 'Point not found.' });
     }
 
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
     
 
-    return res.json(point);
+    return res.json({ point, items });
   }
 
   async create(req: Request, res: Response) {
@@ -53,6 +76,8 @@ class PointsController {
     })
   
     await trx('point_items').insert(pointItems);
+
+    await trx.commit();
   
     return res.json({
       id: point_id,
