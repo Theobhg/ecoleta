05c630ecf588fef10a260c8f6723da67aa8b1792
Theobhg <assis.theo@gmail.com>
2020-06-02 17:36:16 -0300

    create seeds/titles

---
+++ backend/src/database/seeds/create_items.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex';
+
+export async function seed(knex: Knex) {
+  await knex('items').insert([
+    { title: 'Lamp', image: 'lampadas.svg' },
+    { title: 'Batteries', image: 'baterias.svg' },
+    { title: 'Papers and Cardboards', image: 'papeis-papelao.svg' },
+    { title: 'Electronic waste', image: 'eletronicos.svg' },
+    { title: 'organic waste', image: 'organicos.svg' },
+    { title: 'Kitchen oil', image: 'oleo.svg' },
+  ]);
+}
