0617c06d73c6164c7611087df6e0e6f1cc8bc838
Theobhg <assis.theo@gmail.com>
2020-06-03 16:01:53 -0300

    app.css style/google fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Header from './Header';
 
 function App() {
-  let [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-    <Header title={`contador: ${counter}`} />
-
-    <h1>{counter}</h1>
-    <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+    <h1>Hello World</h1>
   );
 }  
 
