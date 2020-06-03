db23206200c854de04feec17414a92cee4a169d2
Theobhg <assis.theo@gmail.com>
2020-06-03 15:56:37 -0300

    starting from the beginning

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
     <div>
-    <Header />
+    <Header title={`contador: ${counter}`} />
 
-    <h1>Hello World</h1>
+    <h1>{counter}</h1>
+    <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }  
