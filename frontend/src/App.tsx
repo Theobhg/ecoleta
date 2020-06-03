abfeebf1cab45e592e0dd6843f4990201c1758c8
Theobhg <assis.theo@gmail.com>
2020-06-03 14:36:03 -0300

    import Header component to func App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
+    <div>
+    <Header />
+
     <h1>Hello World</h1>
+    </div>
   );
 }  
 
