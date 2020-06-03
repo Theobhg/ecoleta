39b6e1b2f7801192dee02b10b2a0a5e81bed02ce
Theobhg <assis.theo@gmail.com>
2020-06-03 17:03:46 -0300

    create html/import FilogIn-reacticons

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="Ecoleta" />
+        </header>
+
+        <main>
+          <h1>Your waste collection marketplace.</h1>
+          <p>We help people find collection points efficiently.</p>
+
+          <a href="/register">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Register a colect point</strong>
+          </a>
+        </main>
+      </div>
+    </div>
   )
 }
 
